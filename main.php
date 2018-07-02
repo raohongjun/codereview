3001faef3d47a25e7168b8fd0e4f44027e8f3a6c
raohongjun <112066440@qq.com>
2018-07-02 10:22:39 +0800

    提交

--- main.php
+++ main.php
@@ -7,35 +7,36 @@
  * Time: 上午11:52
  */
 
-include __DIR__."/vendor/autoload.php";
+include __DIR__ . "/vendor/autoload.php";
+
 use PHPMD\TextUI\Command;
 
-$gitdata=[
-    "医生患者服务平台"=>__DIR__."/../dpserviceplatform",//医生患者服务平台
-    "福瑞医生管理后台"=>__DIR__."/../agAppConsole",//福瑞医生管理后台
-    "福瑞医生会员端"=>__DIR__."/../h5NewSystem",//福瑞医生会员端
-    "福瑞海外医疗小程序服务层"=>__DIR__."/../appletsService",//福瑞海外医疗小程序服务层
-    "问医生小程序"=>__DIR__."/../wxapp-wys",//问医生小程序
-    "问医生小程序测试"=>__DIR__."/../wxapp-wys-test",//问医生小程序测试
-    "权限服务"=>__DIR__."/../permissionService",//权限服务
-    "日志服务"=>__DIR__."/../aglogsService",//日志服务
-    "用户中心服务"=>__DIR__."/../userCenterService",//用户中心服务
-    "脚本服务"=>__DIR__."/../scriptServices",//脚本服务
-    "消息服务层"=>__DIR__."/../messageService",//消息服务层
-    "统计服务层"=>__DIR__."/../statisticsService",//统计服务层
-    "知识库服务层"=>__DIR__."/../loreService",//知识库服务层
-    "搜索服务层"=>__DIR__."/../searchService",//搜索服务层
-    "订单服务层"=>__DIR__."/../orderService",//订单服务层
-    "OCR服务层"=>__DIR__."/../ocrService",//OCR服务层
-    "核心服务层（关）"=>__DIR__."/../coreServices",//核心服务层（关）
-    "肝病联盟管理后台"=>__DIR__."/../businessAdmin",//肝病联盟管理后台
-    "肝病联盟公众号"=>__DIR__."/../h5UninSystem",//肝病联盟公众号
-    "福瑞海外医疗小程序"=>__DIR__."/../wxapp_hwyl",//福瑞海外医疗小程序
-    "302患者端demo"=>__DIR__."/../patientDemo",//302患者端demo
-    "通讯服务"=>__DIR__."/../imservice",//302患者端demo
+$gitdata = [
+    "医生患者服务平台" => __DIR__ . "/../dpserviceplatform",//医生患者服务平台
+    "福瑞医生管理后台" => __DIR__ . "/../agAppConsole",//福瑞医生管理后台
+    "福瑞医生会员端" => __DIR__ . "/../h5NewSystem",//福瑞医生会员端
+    "福瑞海外医疗小程序服务层" => __DIR__ . "/../appletsService",//福瑞海外医疗小程序服务层
+    "问医生小程序" => __DIR__ . "/../wxapp-wys",//问医生小程序
+    "问医生小程序测试" => __DIR__ . "/../wxapp-wys-test",//问医生小程序测试
+    "权限服务" => __DIR__ . "/../permissionService",//权限服务
+    "日志服务" => __DIR__ . "/../aglogsService",//日志服务
+    "用户中心服务" => __DIR__ . "/../userCenterService",//用户中心服务
+    "脚本服务" => __DIR__ . "/../scriptServices",//脚本服务
+    "消息服务层" => __DIR__ . "/../messageService",//消息服务层
+    "统计服务层" => __DIR__ . "/../statisticsService",//统计服务层
+    "知识库服务层" => __DIR__ . "/../loreService",//知识库服务层
+    "搜索服务层" => __DIR__ . "/../searchService",//搜索服务层
+    "订单服务层" => __DIR__ . "/../orderService",//订单服务层
+    "OCR服务层" => __DIR__ . "/../ocrService",//OCR服务层
+    "核心服务层（关）" => __DIR__ . "/../coreServices",//核心服务层（关）
+    "肝病联盟管理后台" => __DIR__ . "/../businessAdmin",//肝病联盟管理后台
+    "肝病联盟公众号" => __DIR__ . "/../h5UninSystem",//肝病联盟公众号
+    "福瑞海外医疗小程序" => __DIR__ . "/../wxapp_hwyl",//福瑞海外医疗小程序
+    "302患者端demo" => __DIR__ . "/../patientDemo",//302患者端demo
+    "通讯服务" => __DIR__ . "/../imservice",//通讯服务
 ];
 
-foreach ($gitdata as $k=>$v) {
+foreach ($gitdata as $k => $v) {
     $git = new \Codereview\gitCount($v);
     $git->addExcludePath([
         "vendor/*",
@@ -53,7 +54,7 @@ foreach ($gitdata as $k=>$v) {
         "helper"
     ]);
     //拉取更新
-    $git->runCommand( "cd " . $v . "&& git pull" );
+    $git->runCommand("cd " . $v . "&& git pull");
 
     $result[$k] = $git->analysis();
 
@@ -67,13 +68,13 @@ foreach ($gitdata as $k=>$v) {
      *  --ignore-violations-on-exit - 即使发现任何违规，也会以零代码退出。
      */
 
-    $conf=[
+    $conf = [
         'php main',
         $v,
         'html',
         'codesize,design,unusedcode',
         '--reportfile',
-        './views/static/'.$k.'.html',
+        './views/static/' . $k . '.html',
         '--exclude',
         'vendor/,Tests/,oss-sdk-php/,thinkphp/,tests/'
     ];
@@ -81,6 +82,6 @@ foreach ($gitdata as $k=>$v) {
     Command::main($conf);
     echo $k;
 }
-file_put_contents("views/data.php",'<?php return $results ='.var_export( $result ,true).';?>');
+file_put_contents("views/data.php", '<?php return $results =' . var_export($result, true) . ';?>');
 
 
